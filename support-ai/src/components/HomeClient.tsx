8ccc4c769da301b68f455554efec84de93a7e3d7
Aneesh <aneeshy1508cse.com>
2026-02-07 13:48:34 +0530

    supportAi

--- support-ai/src/components/HomeClient.tsx
+++ support-ai/src/components/HomeClient.tsx
@@ -1,11 +1,41 @@
 "use client"
-import React from 'react'
-import { motion } from "motion/react"
+import React, { useEffect, useRef, useState } from 'react'
+import { AnimatePresence, motion } from "motion/react"
+import { Inika } from 'next/font/google'
 
-const HomeClient = () => {
-    const handleLogin=()=>{
+
+const HomeClient = ({ email }: { email?: string }) => {
+    const handleLogin = () => {
         window.location.href = "/api/auth/login"
     }
+    // const firstLetter = email[0].toUpperCase();
+    const firstLetter = email?.charAt(0).toUpperCase() || "";
+
+    const [open, setOpen] = useState(false);
+    const popupRef = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        const handler = (e: MouseEvent) => {
+            if (popupRef.current && !popupRef.current.contains(e.target as Node))
+                setOpen(false);
+        }
+        document.addEventListener("mousedown", handler)
+        return () => document.removeEventListener("mousedown", handler)
+    },[])
+
+    const features = [
+        {
+            title :"Plug & Play",
+            desc :"Add the Chatbox to your site with s single script tag. "
+        },
+        {
+            title :"Admin Controlled",
+            desc :"Your Control exactly what the AI knows and answers. "
+        },
+        {
+            title :"Always online",
+            desc :"Your customers get instant support 24/7 "
+        }
+    ]
     return (
         <div className='min-h-screen bg-linear-to-br from-white to bg-zinc-50 text-zinc-900 overflow-x-hidden '>
             <motion.div
@@ -18,17 +48,126 @@ const HomeClient = () => {
             >
                 <div className='max-w-7xl mx-auto px-6 h-16 flex items-center justify-between'>
                     <div className='text-lg font-semibold tracking-tight'>Support <span className='text-zinc-400'>AI</span></div>
-
-                    <button
+                    {email ? <div className='relative' ref={popupRef}>
+                        <button className='w-10 h-10 rounded-full
+                        bg-black text-white flex items-center justify-center
+                        font-semibold
+                        hover:scale-105 transition'
+                            onClick={() => setOpen(!open)}>{firstLetter}</button>
+                        <AnimatePresence>
+                            {open && (
+                                <motion.div
+                                    initial={{ opacity: 0, y: -6 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: -6 }}
+                                    className='absolute right-0 mt-3 w-44 bg-white rounded-xl
+                            shadow-xl border border-zinc-200 overflow-hidden'>
+                                    <button className='w-full text-left px-4 py-3 text-sm hover:bg-zinc-100'>Dashboard</button>
+                                    <button className='block px-4 py-3 text-sm text-red-600 hover:bg-zinc-100'>Logout</button>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
+                    </div> : <button
                         className='px-5 py-2 rounded-full bg-black text-white text-sm font-medium
                 hover:bg-zinc-800 transition disabled:opacity-60 flex items-center gap-2'
-                onClick={handleLogin}
+                        onClick={handleLogin}
                     >
                         Login
                     </button>
+                    }
+
                 </div>
 
             </motion.div>
+            <section className='pt-36 pb-28 px-6'>
+                <div className='max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-20 items-center'>
+                    <motion.div
+                        initial={{ opacity: 0, y: 40 }}
+                        animate={
+                            { opacity: 1, y: 0 }
+                        }
+                        transition={{ duration: 0.7 }}
+                    >
+                        <h1 className='text-4xl md:text-5xl font-semibold leading-tight'>
+                            AI Customer Support <br />
+                            Built for Modern Websites
+                        </h1>
+                        <p className='mt-6 text-lg text-zinc-600 max-w-xl'>
+                            Add a powerful AI Chatbot to your Website in Minutes.
+                            Let your Customers get Instant answers using your own Business knowledge.
+                        </p>
+                        <div className='mt-10 flex gap-4'>
+
+                            {email ?  <button className='px-7 py-3 rounded-xl
+                            bg-black text-white font-medium hover:bg-zinc-800 transition
+                            disabled:opacity-60'>
+                                Go To Dashboard
+                            </button> :
+                             <button className='px-7 py-3 rounded-xl
+                            bg-black text-white font-medium hover:bg-zinc-800 transition
+                            disabled:opacity-60'>
+                                Get Started
+                            </button>
+                            }
+                           
+                            <button className='px-7 py-3 rounded-xl border border-zinc-300 text-zinc-700 hover:bg-zinc-100 transition'>Learn More</button>
+                        </div>
+
+                    </motion.div>
+
+                    <motion.div className='relative'
+                    initial = {{opacity: 0, scale: 0.95}}
+                    animate = {{opacity  : 1, scale: 1}}
+                    transition={{duration : 0.7, delay: 0.2}}
+
+                    >
+                        <div className='rounded-2xl bg-white shadow-2xl border border-zinc-200 p-6'>
+                            <div className='text-sm text-zinc-500 mb-3'>Live Chat Preview</div>
+                            <div className='space-y-3'>
+                                <div className='bg-black text-white rounded-lg px-4 py-2 text-sm ml-auto w-fit'>
+                                    Do you offer Cash On delievery?
+                                </div>
+                                <div className='bg-zinc-100 rounded-lg px-4 py-2 text-sm w-fit'>
+                                    Yes,Cash on Delievery is available
+                                </div>
+                            </div>
+                            <motion.div
+                            animate={{y: [0, -12, 0]}}
+                            transition={{repeat : Infinity, duration: 3}}
+                            className='absolute -bottom-6 -right-6
+                            w-14 h-14 rounded-full bg-black text-white
+                            flex items-center justify-center shadow-xl'
+                            >
+                                💬
+                            </motion.div>
+
+                        </div>
+
+                    </motion.div>
+
+                </div>
+
+            </section>
+            <section 
+            id='feature'
+            className='bg-zinc-50 py-28 px-6 border-t border-zinc-200'>
+                <div className='max-w-5xl mx-auto'>
+                    <motion.h2
+                    initial ={{opacity:0 ,y:20}}
+                    whileInView={{opacity: 1, y:0}}
+                    viewport={{once: false}}
+                    transition={{duration: 0.5}}
+                    className='text-3xl font-semibold text-center'
+                    >
+                        Why Businesses Choose SupportAI
+                        
+                    </motion.h2>
+                    <div>
+
+                    </div>
+                </div>
+
+            </section>
 
         </div>
     )
