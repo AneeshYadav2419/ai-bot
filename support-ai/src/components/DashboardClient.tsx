2477f73e7783d8041ef77afa43cb390e11126865
Aneesh <aneeshy1508cse.com>
2026-02-16 00:39:04 +0530

    Complete

--- support-ai/src/components/DashboardClient.tsx
+++ support-ai/src/components/DashboardClient.tsx
@@ -62,7 +62,7 @@ function DashboardClient({ ownerId }: { ownerId: string }) {
                 <div className='max-w-7xl mx-auto px-6 h-16 flex items-center justify-between'>
                     <div className='text-lg font-semibold tracking-tight' onClick={() => navigate.push("/")}>Support <span className='text-zinc-400'>AI</span></div>
                     <button className='px-4 py-2 rounded-lg border border-zinc-300 text-sm hover:bg-zinc-100
-                    transition'>Embed ChatBox
+                    transition' onClick={() => navigate.push("/embed")}>Embed ChatBox
 
                     </button>
 
--- support-ai/src/components/HomeClient.tsx
+++ support-ai/src/components/HomeClient.tsx
@@ -7,7 +7,9 @@ import { useRouter } from "next/navigation"
 
 
 const HomeClient = ({ email }: { email?: string }) => {
+    const [loading, setLoading]  = useState(false)
     const handleLogin = () => {
+        setLoading(true)
         window.location.href = "/api/auth/login"
     }
     // const firstLetter = email[0].toUpperCase();
@@ -89,8 +91,10 @@ const HomeClient = ({ email }: { email?: string }) => {
                         className='px-5 py-2 rounded-full bg-black text-white text-sm font-medium
                 hover:bg-zinc-800 transition disabled:opacity-60 flex items-center gap-2'
                         onClick={handleLogin}
+                        disabled={loading}
                     >
-                        Login
+                        {loading ? "Loading..." :"Login"}
+        
                     </button>
                     }
 
